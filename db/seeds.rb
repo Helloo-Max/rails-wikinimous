35f45d50d7d3311a6aefa9ec0fe2b9a80166548b
Maxim Wolotschij <m.wolotschij@web.de>
2021-10-12 22:39:49 +0200

    generate seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+num = 0
+
+print 'Generating 10 fake articles'
+
+3.times do
+  print '.'
+  sleep 0.3
+end
+
+puts '' # Empty line
+
+10.times do
+  num += 1
+
+  title = Faker::Books::Dune.title
+  content = Faker::Books::Dune.quote
+
+  article = Article.new(title: title, content: content)
+  article.save
+
+  puts ''
+  puts "#{num} - Generated Article with title: \"#{title}\" and content: \"#{content}\""
+  sleep 0.2
+end
